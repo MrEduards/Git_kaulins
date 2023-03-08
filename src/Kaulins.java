6c812737bab3ae0cec0aef43397e425e0bd6fc37
Vinokurovs <pasts@pasts.lv>
2023-03-08 08:27:33 +0200

    Ieviesta ispēja litotājam ievadīt, cik reizes kauliņiem

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
-	public static void main(String[] args) {
-		int skaitlis;
+	public static void main(String[] args){
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
+		Scanner scan = new Scannner(System.in);
+		System.out.println("Cik reizes metîsi kauliòu?");
+		
+		skaitlis = rand.nextInt();
+		for(int i=1; i<=reizes; i++){
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 
+		}
+		scan.close();
 	}
-
+	
+		
 }
+
