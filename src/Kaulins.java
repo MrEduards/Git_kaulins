30872a62b818a06dc4ef33e0c4cdedb1dd63e1bb
Vinokurovs <pasts@pasts.lv>
2023-03-10 08:25:24 +0200

    Realizta programma atbilstoi prasbm

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 
 public class Kaulins {
 	public static void main(String[] args){
-		int skaitlis, reizes;
+		int skaitlis, reizes =1;
 		Random rand = new Random();
-		Scanner scan = new Scannner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes metîsi kauliòu?");
-		
 		skaitlis = rand.nextInt(6)+1;
 		for(int i=1; i<=reizes; i++){
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
