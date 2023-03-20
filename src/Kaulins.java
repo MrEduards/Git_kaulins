ac1b8620b3455f43cf4e177d55dcbd66d124a03a
Vinokurovs <pasts@pasts.lv>
2023-03-20 16:05:44 +0200

    Lokalajā eZara papildināts programma

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class Kaulins {
 	public static void main(String[] args){
-		int skaitlis, reizes =1;
+		int skaitlis, lielakais = 0, reizes =1;
 		Random rand = new Random();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes metîsi kauliòu?");
 		skaitlis = rand.nextInt(6)+1;
 		for(int i=1; i<=reizes; i++){
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-
+         if(lielakais<skaitlis)
+        	 lielakais = skaitlis;
+         
 		}
+		System.out.println("Lielâkais skaitlis metienos ir"+lielakais);
+		
+				
 		scan.close();
 	}
 	
