0f2133b8b30e36f6dac33e13e5c5ba9265821c37
Vinokurovs <pasts@pasts.lv>
2023-03-21 08:54:07 +0200

    Iespēja uzmest vairākas reizes

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 
 public class Kaulins {
 	public static void main(String[] args){
-		int skaitlis, reizes =1;
+		int skaitlis, reizes = 3;
 		Random rand = new Random();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes metîsi kauliòu?");
 		skaitlis = rand.nextInt(6)+1;
 		for(int i=1; i<=reizes; i++){
+			
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-System.out.println("\n");
+        
+		System.out.println("\n");
 		}
 		scan.close();
 	}
