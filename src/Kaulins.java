260aecc6528015e137665363b7bf8c3cb7838d74
Vinokurovs <pasts@pasts.lv>
2023-03-21 09:12:03 +0200

    Programm tiek parveidota ar vēl divām opcijām

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,21 +2,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Kaulins {
-	public static void main(String[] args){
-		int skaitlis, reizes = 3;
+	static int mestKaulinu(int reizes){
+		int skaitlis;
 		Random rand = new Random();
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Cik reizes metîsi kauliòu?");
-		skaitlis = rand.nextInt(6)+1;
-		for(int i=1; i<=reizes; i++){
-			
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
-        
-		System.out.println("\n");
+		for (int i=1; i<skaitlis; i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzskrita skaitlis "+skaitlis);
 		}
-		scan.close();
-	}
-	
+		public static void main(String[] args){
+			int reizes, izvele, pedejais;
+			Scanner scan = new Scanner(System.in);
+			do{
+				System.out.println("1-Mest kauliò |2-Apskatît pedejo |3-Apturçt");
+			izvele =scan.nextInt();
+			switch(izvele){
+			case 1:
+				do{
+					System.out.println("Cik reizes mest kauliòu?");
+				}while(reizes<1);
+			pedejais = mestKaulinu(reizes);
+			break;
+			
+			case 2:
+				System.out.println("Pedejais uzmestais ir "+pedejais);
+				break;
+			case 3:
+				System.out.println("Programma apturçta");
+				break;
+			}
+			}while(izvele!=2);
+			scan.close();
+			}
 		
-}
+		
+		}
 
+ 
+			
+		
+		
+		
+		
